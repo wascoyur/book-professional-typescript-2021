7746425168227c55320e1932f7113bb6da8b70a5
wascoyur <mytem20007@gmail.com>
2022-05-15 20:57:37 +0500

    true start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const fetchWithAuth = (
+    id: string | number | boolean,
+  ): string | number | boolean => {
+    if (typeof id === 'string') {
+      return id.toLowerCase();
+    }
+    return id;
+  };
+  const out = fetchWithAuth(5);
   return (
     <div className='App'>
       <h1>Ready to develop</h1>
+      <div>{out}</div>
     </div>
   );
 }
